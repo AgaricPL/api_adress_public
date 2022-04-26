7c25be262577f362f3b7c9ad37a3903c013a48dd
bedraf <rafal.bednarek@samlerhuset.com.pl>
2022-04-26 10:16:42 +0200

    fix: problem with IDE if City:where try City:query()->where(...

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
 {
